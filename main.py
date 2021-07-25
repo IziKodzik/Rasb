54f907dd5c8d2e78713f8c9cb918e1756fbb345d
unknown <izikodzik@gmail.com>
2021-07-25 16:12:48 +0100

    test

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from time import sleep
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('xd')
+    print('xddd')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
