a9357f657d46fb7fc5da1818b8c6ba5e2629d5a0
unknown <izikodzik@gmail.com>
2021-07-25 16:13:53 +0100

    test

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from gpiozero import LED
 from time import sleep
 
+led = LED(17)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print('xddd')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while True:
+    print('tftf')
