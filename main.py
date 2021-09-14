128ec807f232f8a8b57d87a57036c01da079bc0c
disunio <s19092@pjwstk.edu.pl>
2021-09-14 22:26:55 +0100

    not important

--- main.py
+++ main.py
@@ -30,7 +30,9 @@ def raspberry_program():
     button.hold_time = 1
     last_button_state = button.is_active
     while True:
-        print(button.is_active)
+        if (not last_button_state) and button.is_active:
+            print(button.is_active)
+
 
 if __name__ == '__main__':
     note_boot()
