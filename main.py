65b0479e51218b231b817ca7a3b863c7f92044e5
disunio <s19092@pjwstk.edu.pl>
2021-09-14 18:26:15 +0100

    s

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-stre = f"boot{datetime.now()}"
-print(stre)
-stre = stre.replace('-', '_')
-stre = stre.replace('.', '_')
-stre = stre.replace(':', '_')
-
-stre = stre.replace(" ","")
-print(stre)
-f = open(stre, "x")
-f.close()
+if __name__ == '__main__':
+    stre = f"boot{datetime.now()}"
+    print(stre)
+    stre = stre.replace('-', '_')
+    stre = stre.replace('.', '_')
+    stre = stre.replace(':', '_')
+    stre = stre.replace(" ", "")
+    print(stre)
+    f = open(stre, "x")
+    f.close()
