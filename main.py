caea48c0a3ce74dc17e07f6e4edce3c207fc89c9
disunio <s19092@pjwstk.edu.pl>
2021-09-14 18:46:46 +0100

    not important

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from datetime import datetime
 if __name__ == '__main__':
     boot_file_name = f"/home/pi/Desktop/boots/boots.txt"
     f = open(boot_file_name, "a")
-    f.write(f"{datetime.now()}")
+    f.write(f"{datetime.now()}\n")
     f.close()
