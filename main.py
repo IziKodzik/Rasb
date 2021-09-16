572cdbdd006525361a5e3b72c68f607161c868a6
IziKodzik <ITI-guuy@gmail.com>
2021-09-17 00:27:43 +0200

    t

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import signal
 import subprocess
 import threading
 from datetime import datetime
@@ -44,8 +45,7 @@ def raspberry_program():
         proc = subprocess.Popen('sudo python3 /home/pi/Desktop/work/raspb-controller/main.py', shell=True,
                                 preexec_fn=os.setsid)
         button.wait_for_inactive()
-        proc.terminate()
-
+        os.killpg(proc.pid, signal.SIGTERM)
 
 if __name__ == '__main__':
     note_boot()
