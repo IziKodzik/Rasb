1285f1e7acd09334c0db1446895e8b792ba6646b
IziKodzik <ITI-guuy@gmail.com>
2021-09-17 01:46:15 +0200

    t

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def raspberry_program():
         proc = subprocess.Popen('sudo python3 /home/pi/Desktop/work/raspb-controller/main.py', shell=True,
                                 preexec_fn=os.setsid)
         str = os.popen('ps -aux | grep \'sudo python3 /home/pi/Desktop/work/raspb-controller/main.py\'').read()
-        print(str.find('root'))
+        print(str[str.find('root'): -1])
         button.wait_for_inactive()
         #TODO ask how to avoid +1 coz its dangerous
         os.system(f'sudo kill {proc.pid + 1}')
